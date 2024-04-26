7a92df6cfd9db9dd065adfbde3f6f854fd9bc340
Valdis Saulespurens <valdis.s.coding@gmail.com>
2024-04-26 14:49:41 +0300

    added simple while loop example

---
+++ scripts/day11.js
@@ -0,0 +1,10 @@
+console.log("Let's do some looping!");
+
+//while loop
+let i = 0; //start value
+while (i < 5) { //condition
+    console.log("While loop iteration", i);
+    i++; //increment i by one //same as i+=1 or i=i+1
+}
+//after iteration
+console.log("After while loop i is", i);
