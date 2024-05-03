fcee01f5ce859f3c78145b16fd78841c0e507f69
Valdis Saulespurens <valdis.s.coding@gmail.com>
2024-05-03 14:49:26 +0300

    single greeting

---
+++ scripts/day12.js
@@ -0,0 +1,19 @@
+//let's explore Javascript functions
+//functions are a way to group code together
+
+//let's make a simple greeting that prints something fixed to console
+function greet() {
+    //so we have a way of grouping code together
+    console.log("Hello there!");
+    console.log("I hope you are having a great day!");
+    //we can add as much code as we want here
+    //still functions should be kept small and do one thing and do it well - this is a good practice
+    //generally if your function is over one page long you should consider breaking it up
+}
+
+//in order to use a function I need to call it
+greet();
+// //I could call this function as many times as I want
+// for (let i = 0; i < 5; i++) {
+//     greet();
+// }
